f11308f0dfb5e0929b04cbffbdf8314547d554ef
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-08-30 17:14:16 +0100

    first jsx

---
+++ MERN stack/Day 3/firstjsx/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello Dojo!</h1>
+    <h2>Things I need TO learn</h2>
+
+    <ul>
+      <li>Learn React</li>
+      <li>Clim Mt .Everst</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;

c2cc60e728fe49cebd5199340e6b1dbdaa6bc44b
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-10-02 14:47:06 +0100

    First Java Program Assignment

---
+++ JAVA stack/Java Fundamentals/First Java Program/Assignment.java
@@ -0,0 +1,8 @@
+public class Assignment {
+public static void main(String [] args){
+
+    System.out.println("My name is Ahmed Zoubair Belhout ");
+    System.out.println("I am 26 Years old ");
+    System.out.println("My Hometown is Hammam-lif");
+}
+}

d1b0f5346436f3d9203d1f72219f0003128e944d
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-10-06 16:20:04 +0100

    Delete daikichi routes

---
+++ JAVA stack/Java Spring/daikichiRoute/src/main/java/com/ahmedbelhout/daikichiroutes/DaikichiRouteApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedbelhout.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRouteApplication.class, args);
+	}
+
+}
